a4ce20f23b221f5e0e70954ac6a642f576722a72
jlopezcur <jlopezcur@users.noreply.github.com>
2020-11-03 20:03:45 +0000

    deploy: f80a5e6c33741967d95c3bd5e5499b4c456f8959

--- component---src-pages-biocv-js-93715f1845b019f08b96.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"G3S+":function(e,t,n){"use strict";n.r(t),n.d(t,"query",(function(){return o}));var a=n("q1tI"),r=n.n(a),l=n("L6Je"),u=n("hYuR"),o="864860772";t.default=function(e){var t=e.data;return r.a.createElement(l.a,null,r.a.createElement(u.a,{title:"Bio/CV"}),r.a.createElement("h2",null,t.markdownRemark.frontmatter.title),r.a.createElement("div",{dangerouslySetInnerHTML:{__html:t.markdownRemark.html}}))}}}]);
-//# sourceMappingURL=component---src-pages-biocv-js-93715f1845b019f08b96.js.map
